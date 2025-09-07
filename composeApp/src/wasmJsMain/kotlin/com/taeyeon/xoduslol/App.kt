9e1e1df81988f37b05afc04a3bc8769dd6632e80
jtaeyeon05 <jtaeyeon05@gmail.com>
2025-09-08 01:58:15 +0900

    Initial Commit [2]

--- composeApp/src/wasmJsMain/kotlin/com/taeyeon/xoduslol/App.kt
+++ composeApp/src/wasmJsMain/kotlin/com/taeyeon/xoduslol/App.kt
@@ -1,27 +1,32 @@
 package com.taeyeon.xoduslol
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.sp
+import org.jetbrains.compose.resources.Font
+import xoduslol.composeapp.generated.resources.Galmuri11
+import xoduslol.composeapp.generated.resources.Galmuri11_Bold
+import xoduslol.composeapp.generated.resources.Galmuri11_Condensed
 import xoduslol.composeapp.generated.resources.Res
-import xoduslol.composeapp.generated.resources.compose_multiplatform
+
 
 @Composable
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
         Column(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.primaryContainer)
@@ -29,18 +34,40 @@ fun App() {
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+            val galmuri11 = FontFamily(
+                Font(
+                    resource = Res.font.Galmuri11,
+                    weight = FontWeight.Normal,
+                    style = FontStyle.Normal,
+                ),
+                Font(
+                    resource = Res.font.Galmuri11_Bold,
+                    weight = FontWeight.Bold,
+                    style = FontStyle.Normal,
+                ),
+                Font(
+                    resource = Res.font.Galmuri11_Condensed,
+                    weight = FontWeight.Light,
+                    style = FontStyle.Normal,
+                ),
+            )
+
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ) {
+                Text(
+                    text = buildAnnotatedString {
+                        append("Hi, It's ")
+                        withStyle(
+                            style = SpanStyle(fontWeight = FontWeight.Bold)
+                        ) {
+                            append("XODUS.LOL")
+                        }
+                    },
+                    fontFamily = galmuri11,
+                    fontSize = 30.sp,
+                )
             }
         }
     }
