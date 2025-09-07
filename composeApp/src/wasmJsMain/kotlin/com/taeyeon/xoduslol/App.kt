76b1fa9edb91f03545c9c3ca7a88d642e0b85277
jtaeyeon05 <jtaeyeon05@gmail.com>
2025-09-08 04:19:02 +0900

    Theme

--- composeApp/src/wasmJsMain/kotlin/com/taeyeon/xoduslol/App.kt
+++ composeApp/src/wasmJsMain/kotlin/com/taeyeon/xoduslol/App.kt
@@ -1,59 +1,31 @@
 package com.taeyeon.xoduslol
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.sp
-import org.jetbrains.compose.resources.Font
-import xoduslol.composeapp.generated.resources.Galmuri11
-import xoduslol.composeapp.generated.resources.Galmuri11_Bold
-import xoduslol.composeapp.generated.resources.Galmuri11_Condensed
-import xoduslol.composeapp.generated.resources.Res
+import com.taeyeon.xoduslol.ui.AppTheme
 
 
 @Composable
 fun App() {
-    MaterialTheme {
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            val galmuri11 = FontFamily(
-                Font(
-                    resource = Res.font.Galmuri11,
-                    weight = FontWeight.Normal,
-                    style = FontStyle.Normal,
-                ),
-                Font(
-                    resource = Res.font.Galmuri11_Bold,
-                    weight = FontWeight.Bold,
-                    style = FontStyle.Normal,
-                ),
-                Font(
-                    resource = Res.font.Galmuri11_Condensed,
-                    weight = FontWeight.Light,
-                    style = FontStyle.Normal,
-                ),
-            )
-
+    AppTheme(
+        darkTheme = false
+    ) {
+        Surface {
             Box(
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .safeContentPadding(),
                 contentAlignment = Alignment.Center,
             ) {
                 Text(
@@ -65,7 +37,6 @@ fun App() {
                             append("XODUS.LOL")
                         }
                     },
-                    fontFamily = galmuri11,
                     fontSize = 30.sp,
                 )
             }
